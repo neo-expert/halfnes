8d35d57ae216a9e73be67dbc419044aa3e48766b
neoexpert <neoexpert@gmail.com>
2020-05-18 22:10:05 +0000

    DOMHalfnes error reporting

--- DOMHalfnes/src/main/java/com/neoexpert/domhalfnes/DOMNES.java
+++ DOMHalfnes/src/main/java/com/neoexpert/domhalfnes/DOMNES.java
@@ -7,6 +7,11 @@ import js.dom.DOM;
 
 public class DOMNES {
     public static void main(String ... args){
+			if(!DOM.isInitialized()){
+				System.out.println("ERROR: DOM is not initialized");
+				return;
+			}
+
 			APU.setDedaultAudioInterface(new DOMAudio());
         PrefsSingleton.set(new DOMPrefs());
         DOMUI ui=new DOMUI();
