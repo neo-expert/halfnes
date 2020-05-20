cd7895556efaaa0b70352deecc59b17a5ac3d4bc
neoexpert <neoexpert@gmail.com>
2020-05-20 17:55:06 +0200

    gitignore

--- DOMHalfnes/src/main/java/com/neoexpert/domhalfnes/DOMRenderer.java
+++ DOMHalfnes/src/main/java/com/neoexpert/domhalfnes/DOMRenderer.java
@@ -8,6 +8,7 @@ import js.dom.ImageData;
 public class DOMRenderer extends Renderer{
     private final Canvas canvas;
     private final Canvas.Context2D ctx;
+    private int last_pixel;
 
     public DOMRenderer(Canvas canvas){
         //frame_width=256;
@@ -17,12 +18,18 @@ public class DOMRenderer extends Renderer{
 
     @Override
     public void render(int[] nespixels, int[] bgcolors, boolean dotcrawl) {
+        int pixel=this.last_pixel;
         for (int i = 0; i < nespixels.length; ++i) {
-            nespixels[i] = NesColors.col[(nespixels[i] & 0x1c0) >> 6][nespixels[i] & 0x3f];
+            int p= NesColors.col[(nespixels[i] & 0x1c0) >> 6][nespixels[i] & 0x3f];
+            nespixels[i] = p;
+            if(p!=pixel){
+                System.out.println("pixel: "+pixel);
+                pixel=p;
+            }
         }
+        last_pixel=pixel;
         ImageData imageData = ctx.createImageData(256, 240);
         imageData.setIntArray(nespixels);
         ctx.putImageData(0,0,imageData);
-        System.out.println("nespixels length:"+nespixels.length);
     }
 }
